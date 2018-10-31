7fa8efa18b7fa4aafe2ac618d2ea5841f355ac64
Vinicius José Ribeiro de Oliveira <viniciusjose.19@gmail.com>
2018-10-31 15:54:43 -0300

    Versão FInal - implementações e ajustes adicionais
    Novas atualizações

---
+++ egressas&egressos/paginas_remover/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="materialize/css/materialize.min.css"  media="screen,projection"/>
+      <link rel="stylesheet" type="text/css" href="css/custom.css">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+
+    <nav class="sidebar-custom">
+    	<div class="nav-wrapper container">
+
+		<i href="#" data-target="slide-out" class="material-icons left sidenav-trigger">menu</i>
+    	<a href="index.php" class="center brand-logo">IFJR</a>
+		</div>
+    </nav>
+  
+  	<ul id="slide-out" class="sidenav">
+		<li><div class="user-view">
+		<div class="background sidenav-custom"><!--<img src="images/office.jpg">!--></div>
+		<a href="#"><img class="circle" src="img/pessoa-icon.png"></a>
+		<a href="#"><span class="white-text name">Matheus Giovanni Lucena</span></a>
+		<a href="#"><span class="white-text email">matheus.giovanni.lu@gmail.com</span></a>
+		</div></li>
+		<li><a class="subheader">Sistemas</a></li>
+		<li><a href="https://webmail.ifjrsolucoes.com.br/cpsess9257546587/webmail/paper_lantern/index.html?login=1&post_login=19766284951649" target="blank"><i class="material-icons">email</i>WebMail</a></li>
+		<li><div class="divider"></div></li>
+		<li><a class="subheader">Cursos</a></li>
+		<li><a href="#" class="waves-effect"><i class="material-icons">time_to_leave</i>Engenharia</a></li>
+		<li><a href="#" class="waves-effect"><i class="material-icons">tablet_android</i>ADS</a></li>
+		<li><a class="subheader">Projetos</a></li>
+		<li><a href="#" class="waves-effect"><i class="material-icons">android</i>Dispositivos Móveis</a></li>
+		<li><a href="#" class="waves-effect"><i class="material-icons">web</i>WEB</a></li>
+		<li><a href="#" class="waves-effect"><i class="material-icons">settings</i>Engenharia</a></li>
+		<li><div class="divider"></div></li>
+		<li><a class="subheader">Finanças</a></li>
+		<li><a href="#" class="waves-effect"><i class="material-icons">attach_money</i>Saque em caixa</a></li>
+		<li><a href="#" class="waves-effect"><i class="material-icons">account_balance_wallet</i>Saldo</a></li>
+		<li><a href="#" class="waves-effect"><i class="material-icons">account_balance</i>Depósito</a></li>
+		<li><div class="divider"></div></li>
+		<li><a class="subheader">Conta</a></li>
+		<li><a href="#" class="waves-effect"><i class="material-icons">security</i>Trocar Senha</a></li>
+	</ul>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+      <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+      <script type="text/javascript" src="js/custom.js"></script>
+    </body>
+  </html>
